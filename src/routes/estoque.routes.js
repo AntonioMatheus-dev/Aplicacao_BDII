46466596c0ba36bb24a06aab7787213a3a3973f9
AntonioMatheus-dev <matheus69969@gmail.com>
2026-01-23 15:21:58 -0300

    feat: adcionando rotas para estoque

--- src/routes/estoque.routes.js
+++ src/routes/estoque.routes.js
@@ -4,6 +4,7 @@ import EstoqueController from '../app/controllers/EstoqueController.js'
 
 const router = Router();
 
+
 router.get("/estoque-baixo", (req, res) => EstoqueController.estoqueBaixo(req, res));
 router.get("/produtos", (req, res) => EstoqueController.listarProdutos(req, res));
 router.get("/historico/:produtoId", (req, res) => EstoqueController.historico(req, res));
