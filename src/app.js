c0bdf6f182cd8a4a277abf636e38dc332c2d3e86
AntonioMatheus-dev <matheus69969@gmail.com>
2026-01-25 13:37:28 -0300

    feat: Establish initial web application structure with client-side routing, multiple management views, and data repositories.

--- src/app.js
+++ src/app.js
@@ -5,6 +5,14 @@ import routes from "./routes/index.js"; // estava ./app/routes...
 const app = express();
 app.use(express.json());
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Rota raiz com documentação
 
 app.use(routes);
