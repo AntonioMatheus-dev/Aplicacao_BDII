ece8e34a6fc491357aec8228709593a5a53a45c0
AntonioMatheus-dev <matheus69969@gmail.com>
2026-01-25 15:07:20 -0300

    feat: add PostgreSQL database connection pool and query utility.

--- src/app/database/conexao.js
+++ src/app/database/conexao.js
@@ -2,11 +2,11 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
-  host: "localhost",
-  port: 5432,
-  user: "postgres",
-  password: "mat.1127",
-  database: "Trabalho_estoque",
+  host: process.env.DB_HOST || "localhost",
+  port: process.env.DB_PORT || 5432,
+  user: process.env.DB_USER || "postgres",
+  password: process.env.DB_PASSWORD || "mat.1127",
+  database: process.env.DB_NAME || "Trabalho_estoque",
 });
 
 pool.on("error", (err) => {
