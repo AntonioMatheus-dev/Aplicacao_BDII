0a07d60d1e0bed8541c8854f520c594047dcf1c6
AntonioMatheus-dev <matheus69969@gmail.com>
2026-01-25 13:37:59 -0300

    feat: Add initial application structure including dashboard, entity management views, comprehensive styling, and data repositories.

---
+++ src/app/database/conexao.js
@@ -0,0 +1,26 @@
+import pkg from "pg";
+const { Pool } = pkg;
+
+const pool = new Pool({
+  host: "localhost",
+  port: 5432,
+  user: "postgres",
+  password: "mat.1127",
+  database: "Trabalho_estoque",
+});
+
+pool.on("error", (err) => {
+  console.error("Erro não esperado no pool:", err);
+});
+
+export const consulta = (sql, valores = [], menssagemReject) => {
+  return new Promise((resolve, reject) => {
+    pool.query(sql, valores, (erro, result) => {
+      if (erro) return reject(menssagemReject || erro.message);
+      const row = result.rows;
+      return resolve(row);
+    });
+  });
+};
+
+export default pool;
