fb4ea6b8297a8001f62e846647bf37d182eab781
AntonioMatheus-dev <matheus69969@gmail.com>
2026-01-28 10:57:03 -0300

    feat: Implement PostgreSQL database connection.

--- src/app/database/conexao.js
+++ src/app/database/conexao.js
@@ -2,11 +2,13 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT || 5432,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false 
 });
 
 pool.on("error", (err) => {
