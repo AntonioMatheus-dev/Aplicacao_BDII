4e65eaac037f483b4046502206990421619fa885
AntonioMatheus-dev <matheus69969@gmail.com>
2026-02-05 11:38:14 -0300

    feat(routes): add POST /promote route to fornecedores

--- src/routes/fornecedor.routes.js
+++ src/routes/fornecedor.routes.js
@@ -5,6 +5,7 @@ const router = Router();
 
 router.get("/", (req, res) => FornecedorController.index(req, res));
 router.post("/", (req, res) => FornecedorController.store(req, res));
+router.post("/promote", (req, res) => FornecedorController.promote(req, res));
 router.get("/:id", (req, res) => FornecedorController.show(req, res));
 router.put("/:id", (req, res) => FornecedorController.update(req, res));
 router.delete("/:id", (req, res) => FornecedorController.delete(req, res));
