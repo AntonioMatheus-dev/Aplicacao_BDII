76a5e5f8d843f3e36f546559b01fcc8ea0fef06d
AntonioMatheus-dev <matheus69969@gmail.com>
2026-02-05 11:37:22 -0300

    feat(routes): add POST /promote route to clientes

--- src/routes/clientes.routes.js
+++ src/routes/clientes.routes.js
@@ -5,6 +5,7 @@ const router = Router();
 
 router.get("/", (req, res) => ClienteController.index(req, res));
 router.post("/", (req, res) => ClienteController.store(req, res));
+router.post("/promote", (req, res) => ClienteController.promote(req, res));
 router.get("/:id", (req, res) => ClienteController.show(req, res));
 router.put("/:id", (req, res) => ClienteController.update(req, res));
 router.delete("/:id", (req, res) => ClienteController.delete(req, res));
