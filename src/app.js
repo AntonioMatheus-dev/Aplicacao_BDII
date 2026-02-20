d8d9f84a0c28583e645caddacb12d3d4f0548e10
AntonioMatheus-dev <matheus69969@gmail.com>
2026-02-20 11:13:49 -0300

    refactor(app): remove unused path and URL imports

--- src/app.js
+++ src/app.js
@@ -4,15 +4,6 @@ import routes from "./routes/index.js";
 const app = express();
 app.use(express.json());
 
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-
 app.use(routes);
 
 export default app;
